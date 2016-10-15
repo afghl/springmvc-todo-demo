cc503ddb36bf25fb62257b31e40af3e5dcda6373
afghl <airport.0930@gmail.com>
2016-10-15 17:07:12 +0800

    add classes

---
+++ src/main/java/com/springdemo/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by afghl on 16/10/15.
+ */
+
+@Controller
+@RequestMapping("/hello")
+public class HomeController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String home(ModelMap model) {
+        System.out.println("goto HomeController....");
+        return "test";
+    }
+}
